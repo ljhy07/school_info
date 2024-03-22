b599518c3baae6fde826173b9e4924bddf0f2b3d
ljhy07 <ljhy.0723@gmail.com>
2024-03-22 11:31:37 +0900

    오류 수정

--- cafeteria.py
+++ cafeteria.py
@@ -4,24 +4,26 @@ import re
 from datetime import datetime
 
 class Cafeteria:
-    def __init__(self, school_info):
-        self.school_info = school_info
-        self.yearTime = datetime.now().strftime("%Y")
-        self.monthTime = datetime.now().strftime("%m")
-        self.dayTime = datetime.now().strftime("%d")
-        self.url = f"https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={self.school_info.atpt_code}&SD_SCHUL_CODE={self.school_info.schul_code}&MLSV_YMD={self.yearTime.zfill(4)}{self.monthTime.zfill(2)}{self.dayTime.zfill(2)}"
-
-        self.response_cafe = requests.get(self.url)
-        self.contents_cafe = self.response_cafe.text
-        self.parsed_data_cafe = json.loads(self.contents_cafe)
+    def __init__(self, school_info_instance):
+        self.school_info = school_info_instance
+        
 
     def cafe(self):
-        if self.response_cafe.status_code == 200:
+        yearTime = datetime.now().strftime("%Y")
+        monthTime = datetime.now().strftime("%m")
+        dayTime = datetime.now().strftime("%d")
+        url = f"https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={self.school_info.atpt_code}&SD_SCHUL_CODE={self.school_info.schul_code}&MLSV_YMD={yearTime.zfill(4)}{monthTime.zfill(2)}{dayTime.zfill(2)}"
+
+        response_cafe = requests.get(url)
+        contents_cafe = response_cafe.text
+        parsed_data_cafe = json.loads(contents_cafe)
+    
+        if response_cafe.status_code == 200:
             dish_name = ""
             try:
                 meal_list = []
-                if "mealServiceDietInfo" in self.parsed_data_cafe:
-                    for row in self.parsed_data_cafe["mealServiceDietInfo"][1]["row"]:
+                if "mealServiceDietInfo" in parsed_data_cafe:
+                    for row in parsed_data_cafe["mealServiceDietInfo"][1]["row"]:
                         meal_name = row.get("MMEAL_SC_NM", "NULL")
                         meal_date = row.get("MLSV_YMD", "NULL")
                         dish_name = row.get("DDISH_NM", "NULL")
